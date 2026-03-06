d948716425656f773f4a18ebc5ad999da41ed000
denismrvoljak <107789986+denismrvoljak@users.noreply.github.com>
2026-03-06 17:47:11 +0100

    added unit tests for Exercise and Workout exercise

--- test/models/exercise_test.rb
+++ test/models/exercise_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 class ExerciseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "exercise without name is invalid" do
+    exercise = Exercise.new()
+    assert_not exercise.valid?
+  end
+
+  test "workout exercises with negative weigth is invalid" do
+    workout = Workout.new(name: "Push day", date: Date.today)
+    exercise = Exercise.new(name: "Bench Press")
+    entry = WorkoutExercise.new(workout: workout, exercise: exercise, weight: -1, sets: 3, reps: 8)
+    assert_not entry.valid?
+  end
+  test "workout exercises with negative sets is invalid" do
+    workout = Workout.new(name: "Push day", date: Date.today)
+    exercise = Exercise.new(name: "Bench Press")
+    entry = WorkoutExercise.new(workout: workout, exercise: exercise, weight: 8, sets: -1, reps: 8)
+    assert_not entry.valid?
+  end
+  test "workout exercises with negative reps is invalid" do
+    workout = Workout.new(name: "Push day", date: Date.today)
+    exercise = Exercise.new(name: "Bench Press")
+    entry = WorkoutExercise.new(workout: workout, exercise: exercise, weight: 8, sets: 3, reps: -1)
+    assert_not entry.valid?
+  end
 end
