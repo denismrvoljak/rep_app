9af4a99a7251cb5b061b1ecb4e78f28a8075748a
denismrvoljak <107789986+denismrvoljak@users.noreply.github.com>
2026-03-09 20:43:47 +0100

    feat: clickable workouts, back button, new workout form, basic CSS, model tests

--- test/models/exercise_test.rb
+++ test/models/exercise_test.rb
@@ -5,23 +5,4 @@ class ExerciseTest < ActiveSupport::TestCase
     exercise = Exercise.new()
     assert_not exercise.valid?
   end
-
-  test "workout exercises with negative weigth is invalid" do
-    workout = Workout.new(name: "Push day", date: Date.today)
-    exercise = Exercise.new(name: "Bench Press")
-    entry = WorkoutExercise.new(workout: workout, exercise: exercise, weight: -1, sets: 3, reps: 8)
-    assert_not entry.valid?
-  end
-  test "workout exercises with negative sets is invalid" do
-    workout = Workout.new(name: "Push day", date: Date.today)
-    exercise = Exercise.new(name: "Bench Press")
-    entry = WorkoutExercise.new(workout: workout, exercise: exercise, weight: 8, sets: -1, reps: 8)
-    assert_not entry.valid?
-  end
-  test "workout exercises with negative reps is invalid" do
-    workout = Workout.new(name: "Push day", date: Date.today)
-    exercise = Exercise.new(name: "Bench Press")
-    entry = WorkoutExercise.new(workout: workout, exercise: exercise, weight: 8, sets: 3, reps: -1)
-    assert_not entry.valid?
-  end
 end
--- test/models/workout_exercise_test.rb
+++ test/models/workout_exercise_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class WorkoutExerciseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "workout exercises with negative weight is invalid" do
+    workout = Workout.new(name: "Push day", date: Date.today)
+    exercise = Exercise.new(name: "Bench Press")
+    entry = WorkoutExercise.new(workout: workout, exercise: exercise, weight: -1, sets: 3, reps: 8)
+    assert_not entry.valid?
+  end
+  test "workout exercises with negative sets is invalid" do
+    workout = Workout.new(name: "Push day", date: Date.today)
+    exercise = Exercise.new(name: "Bench Press")
+    entry = WorkoutExercise.new(workout: workout, exercise: exercise, weight: 8, sets: -1, reps: 8)
+    assert_not entry.valid?
+  end
+  test "workout exercises with negative reps is invalid" do
+    workout = Workout.new(name: "Push day", date: Date.today)
+    exercise = Exercise.new(name: "Bench Press")
+    entry = WorkoutExercise.new(workout: workout, exercise: exercise, weight: 8, sets: 3, reps: -1)
+    assert_not entry.valid?
+  end
 end
